5c63bcb5661642e1a3360fdaae8f4a8cf20daf22
Lucas Silva <kucasss19@gmail.com>
2025-01-23 14:29:54 -0300

    adição estrutura inicial spring boot

---
+++ src/test/java/br/com/lucas/oliveira/SistemaBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucas.oliveira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
