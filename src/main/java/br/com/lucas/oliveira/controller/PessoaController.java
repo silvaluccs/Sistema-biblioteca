4f33052ccf70dbba2ca9d8b4121014e0201e3345
Lucas Silva <kucasss19@gmail.com>
2025-02-08 18:36:23 -0300

    add entidade livro

--- src/main/java/br/com/lucas/oliveira/controller/PessoaController.java
+++ src/main/java/br/com/lucas/oliveira/controller/PessoaController.java
@@ -17,56 +17,50 @@ import br.com.lucas.oliveira.service.PessoaService;
 @RequestMapping("/pessoas")
 public class PessoaController {
 
-	@Autowired
-	private PessoaService pessoaService;
-	
-	@GetMapping
-	public ResponseEntity<List<PessoaDTO>> listar() {
-		try {
-			return ResponseEntity.ok(pessoaService.listar());
-		} 
-		catch (EntidadeNulaException e) {
-		    return ResponseEntity.badRequest().header("Erro", e.getMessage()).build();
-		}
-		
-	}
-	
-	@GetMapping("/{pessoaId}")
-	public ResponseEntity<PessoaDTO> buscar(@PathVariable Long pessoaId) {
-		
-		try {
-			return ResponseEntity.ok(pessoaService.buscar(pessoaId));
-		} 
-		catch (EntidadeNulaException e) {
-			return ResponseEntity.notFound().build();
-		}
-		
-	}
-	
-	@GetMapping("{pessoaId}/logado")
-	public ResponseEntity<String> buscarLogado(@PathVariable Long pessoaId) {
+  @Autowired
+  private PessoaService pessoaService;
 
-		try {
-			boolean logado = pessoaService.buscarLogado(pessoaId);
-			return ResponseEntity.ok(String.format("Pessoa %s logada", logado ? "está" : "não está"));
-		} catch (EntidadeNulaException e) {
-			return ResponseEntity.notFound().build();
-		}
+  @GetMapping
+  public ResponseEntity<List<PessoaDTO>> listar() {
+    try {
+      return ResponseEntity.ok(pessoaService.listar());
+    } catch (EntidadeNulaException e) {
+      return ResponseEntity.badRequest().header("Erro", e.getMessage()).build();
+    }
 
-	}
-	
-	
-	@GetMapping("/logados/{statusLogadoProcurado}")
-	public ResponseEntity<List<PessoaDTO>> buscarLogados(@PathVariable boolean statusLogadoProcurado) {
-		
-		try {
-			return ResponseEntity.ok(pessoaService.buscarLogados(statusLogadoProcurado));
-		}
-		catch (EntidadeNulaException e) {
-			return ResponseEntity.notFound().build();
-		}
-		
-	}
+  }
+
+  @GetMapping("/{pessoaId}")
+  public ResponseEntity<PessoaDTO> buscar(@PathVariable Long pessoaId) {
+
+    try {
+      return ResponseEntity.ok(pessoaService.buscar(pessoaId));
+    } catch (EntidadeNulaException e) {
+      return ResponseEntity.notFound().build();
+    }
+
+  }
+
+  @GetMapping("{pessoaId}/logado")
+  public ResponseEntity<String> buscarLogado(@PathVariable Long pessoaId) {
+
+    try {
+      boolean logado = pessoaService.buscarLogado(pessoaId);
+      return ResponseEntity.ok(String.format("Pessoa %s logada", logado ? "está" : "não está"));
+    } catch (EntidadeNulaException e) {
+      return ResponseEntity.notFound().build();
+    }
+
+  }
+
+  @GetMapping("/logados/{statusLogadoProcurado}")
+  public ResponseEntity<List<PessoaDTO>> buscarLogados(@PathVariable boolean statusLogadoProcurado) {
+
+    try {
+      return ResponseEntity.ok(pessoaService.buscarLogados(statusLogadoProcurado));
+    } catch (EntidadeNulaException e) {
+      return ResponseEntity.notFound().build();
+    }
+
+  }
 }
-	
-	
--- src/main/java/br/com/lucas/oliveira/model/Pessoa.java
+++ src/main/java/br/com/lucas/oliveira/model/Pessoa.java
@@ -13,24 +13,23 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Pessoa {
-	
-	@EqualsAndHashCode.Include
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	@JoinColumn(nullable = false)
-	private String nome;
-	
-	@JoinColumn(nullable = false)
-	private Integer idade;
-	
-	@JoinColumn(nullable = false)
-	private String cpf;
-	
-	@JoinColumn(nullable = false)
-	@OneToOne
-	private ContaBiblioteca contaBiblioteca;
-	
-	
+
+  @EqualsAndHashCode.Include
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @JoinColumn(nullable = false)
+  private String nome;
+
+  @JoinColumn(nullable = false)
+  private Integer idade;
+
+  @JoinColumn(nullable = false)
+  private String cpf;
+
+  @JoinColumn(nullable = false)
+  @OneToOne
+  private ContaBiblioteca contaBiblioteca;
+
 }
